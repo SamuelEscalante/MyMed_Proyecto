73861908cf11858af5abb3f5c134d6950357715d
saalesgu <saalesgu@gmail.com>
2023-03-27 17:24:15 -0500

    Contraseña MYSQL

--- microCompras/src/models/comprasModel.js
+++ microCompras/src/models/comprasModel.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '1234',
     database: 'inventariomymed'
 });
 async function crearCompra(compra) {
